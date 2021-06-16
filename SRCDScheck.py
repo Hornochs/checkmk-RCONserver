cc7c7aff53f04cd141ad038425c48ea7c13b0826
Hornochs <63955136+Hornochs@users.noreply.github.com>
2021-06-16 14:39:49 +0200

    Create SRCDScheck.py
    Initial Push

---
+++ SRCDScheck.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#pip install pysrcds
+from srcds.rcon import RconConnection
+import re
+#Variablen für Verbindung
+#Checkname aren't allowed to spaces!!!
+checkname = "CSS_Classic"
+host = '192.168.254.194'
+port = '27015'
+rconpass = 'trololo'
+#Connection
+conn = RconConnection(host, port=port, password=rconpass)
+response = conn.exec_command('status')
+out = response.replace('\\n', '\n')
+p = re.compile('players[^\n]*')
+m = p.search(out)
+#Group Text to have only one Line
+serverstats=m.group()
+players = re.findall(r'\d+', serverstats)
+#Print line for CheckMK
+print('P {} Spieler= {};{};{};{};{} {} {} von {} Spieler auf dem Server'.format(checkname, str(players[0]), str(int(players[2])-2), str(players[2]), 0, str(players[2]), checkname, str(players[0]), str(players[2])))
